36f83706695a10d8fc1a18249bb54f7185dfe728
Adebayo Ayomide <adebayormide@gmail.com>
2023-10-13 22:22:25 -0400

    correct mistake

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -1,6 +1,7 @@
 let productModel = require("../models/products");
 
-module.exports.create = async function (req, res, next) {
+// Function to get all products
+module.exports.welcomeMessage = async (req, res) => {
     try {
         res.json({
             success: true,
@@ -8,5 +9,65 @@ module.exports.create = async function (req, res, next) {
         });
     } catch (error) {
         next(error);
+    }
+};
+
+// Function to get all products
+module.exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await productModel.find();
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Function to get a product by ID
+module.exports.getProductById = async (req, res) => {
+    try {
+        const product = await productModel.findById(req.params.id);
+        if (product == null) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Function to add a new product
+module.exports.addProduct = async (req, res) => {
+    const newProduct = new productModel(req.body);
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(201).json(savedProduct);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// Function to update a product by ID
+exports.updateProduct = async (req, res) => {
+    try {
+        const updatedProduct = await productModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (updatedProduct == null) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.json(updatedProduct);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// Function to delete a product by ID
+exports.deleteProduct = async (req, res) => {
+    try {
+        const deletedProduct = await productModel.findByIdAndDelete(req.params.id);
+        if (deletedProduct == null) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
--- app/routes/products.js
+++ app/routes/products.js
@@ -1,11 +1,15 @@
 let express = require('express');
 let router = express.Router();
 
-let dressStoreController = require("../controllers/products.js");
+let productController = require("../controllers/products");
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', productController.welcomeMessage);
+router.get('/products', productController.getAllProducts);
+router.get('/products/:id', productController.getProductById);
+router.post('/products', productController.addProduct);
+router.put('/products/:id', productController.updateProduct);
+router.delete('/products/:id', productController.deleteProduct);
+
 
 module.exports = router;
